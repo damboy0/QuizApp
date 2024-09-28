3f1472c33c969cf4ea5d131230b9fb6642612fc8
damboy0 <fagbolade006@gmail.com>
2024-09-28 12:42:39 +0100

    Updated

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { quizData } from './data'
+
+
+const Quiz = () => {
+
+    
+  return (
+    <>
+      <h1>OOOsa</h1>
+    </>
+  )
+}
+
+export default Quiz
---
+++ src/components/data.js
@@ -0,0 +1,70 @@
+export const quizData = {
+    id: 3,
+    topic: 'Random General Knowledge',
+    level: 'Intermediate',
+    totalQuestions: 10,
+    perQuestionScore: 5,
+    questions: [
+      {
+        question: 'Which planet is known as the Red Planet?',
+        choices: ['Venus', 'Saturn', 'Mars', 'Jupiter'],
+        type: 'MCQs',
+        correctAnswer: 'Mars',
+      },
+      {
+        question: 'Who wrote the novel "1984"?',
+        choices: ['George Orwell', 'Aldous Huxley', 'J.K. Rowling', 'Ernest Hemingway'],
+        type: 'MCQs',
+        correctAnswer: 'George Orwell',
+      },
+      {
+        question: 'In which year did the Titanic sink?',
+        choices: ['1912', '1905', '1920', '1898'],
+        type: 'MCQs',
+        correctAnswer: '1912',
+      },
+      {
+        question: 'What is the capital of Australia?',
+        choices: ['Sydney', 'Melbourne', 'Canberra', 'Brisbane'],
+        type: 'MCQs',
+        correctAnswer: 'Canberra',
+      },
+      {
+        question: 'Which chemical element has the symbol O?',
+        choices: ['Oxygen', 'Osmium', 'Oganesson', 'Oxide'],
+        type: 'MCQs',
+        correctAnswer: 'Oxygen',
+      },
+      {
+        question: 'Who painted the Mona Lisa?',
+        choices: ['Leonardo da Vinci', 'Vincent van Gogh', 'Pablo Picasso', 'Claude Monet'],
+        type: 'MCQs',
+        correctAnswer: 'Leonardo da Vinci',
+      },
+      {
+        question: 'Which is the smallest country in the world?',
+        choices: ['Monaco', 'Vatican City', 'San Marino', 'Liechtenstein'],
+        type: 'MCQs',
+        correctAnswer: 'Vatican City',
+      },
+      {
+        question: 'What is the largest organ in the human body?',
+        choices: ['Heart', 'Skin', 'Lungs', 'Liver'],
+        type: 'MCQs',
+        correctAnswer: 'Skin',
+      },
+      {
+        question: 'Who is known as the "Father of Computers"?',
+        choices: ['Charles Babbage', 'Alan Turing', 'Steve Jobs', 'Bill Gates'],
+        type: 'MCQs',
+        correctAnswer: 'Charles Babbage',
+      },
+      {
+        question: 'In what year was the first iPhone released?',
+        choices: ['2005', '2007', '2008', '2010'],
+        type: 'MCQs',
+        correctAnswer: '2007',
+      }
+    ],
+  }
+  
